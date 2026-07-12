7955251edd27e4840ab8e59f7a193139bacf1325
ontowhee <82607723+ontowhee@users.noreply.github.com>
2026-07-12 08:33:51 -0700

    ticket-25243: Added sqlite3 dump using schema from bug report.

--- books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, create_book
-
-app_name = 'books'
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('create/', create_book, name='create'),
-]
--- config/settings.py
+++ config/settings.py
@@ -37,13 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'books',
 ]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
